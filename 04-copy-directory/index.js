cc4eba990534693782ce149590ba82c4702c799e
Evalion8 <alisultanovaeva@gmail.com>
2025-01-21 16:12:54 +0300

    "feat: add solution 5 task"

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,47 +1,47 @@
-// const fs = require('fs');
-// const path = require('path');
+const fs = require('fs');
+const path = require('path');
 
-// async function copyDir() {
-//   const sourceDir = path.join(__dirname, 'files');
-//   const destinationDir = path.join(__dirname, 'files-copy');
+async function copyDir() {
+  const sourceDir = path.join(__dirname, 'files');
+  const destinationDir = path.join(__dirname, 'files-copy');
 
-//   try {
-//     await fs.promises.mkdir(destinationDir, { recursive: true });
+  try {
+    await fs.promises.mkdir(destinationDir, { recursive: true });
 
-//     const items = await fs.promises.readdir(sourceDir, { withFileTypes: true });
+    const items = await fs.promises.readdir(sourceDir, { withFileTypes: true });
 
-//     for (const item of items) {
-//       const sourcePath = path.join(sourceDir, item.name);
-//       const destinationPath = path.join(destinationDir, item.name);
+    for (const item of items) {
+      const sourcePath = path.join(sourceDir, item.name);
+      const destinationPath = path.join(destinationDir, item.name);
 
-//       if (item.isDirectory()) {
-//         await copyDirRecursive(sourcePath, destinationPath);
-//       } else if (item.isFile()) {
-//         await fs.promises.copyFile(sourcePath, destinationPath);
-//       }
-//     }
+      if (item.isDirectory()) {
+        await copyDirRecursive(sourcePath, destinationPath);
+      } else if (item.isFile()) {
+        await fs.promises.copyFile(sourcePath, destinationPath);
+      }
+    }
 
-//     console.log('Directory copied successfully!');
-//   } catch (error) {
-//     console.error('Error copying directory:', error);
-//   }
-// }
+    console.log('Directory copied successfully!');
+  } catch (error) {
+    console.error('Error copying directory:', error);
+  }
+}
 
-// async function copyDirRecursive(sourceDir, destinationDir) {
-//   await fs.promises.mkdir(destinationDir, { recursive: true });
+async function copyDirRecursive(sourceDir, destinationDir) {
+  await fs.promises.mkdir(destinationDir, { recursive: true });
 
-//   const items = await fs.promises.readdir(sourceDir, { withFileTypes: true });
+  const items = await fs.promises.readdir(sourceDir, { withFileTypes: true });
 
-//   for (const item of items) {
-//     const sourcePath = path.join(sourceDir, item.name);
-//     const destinationPath = path.join(destinationDir, item.name);
+  for (const item of items) {
+    const sourcePath = path.join(sourceDir, item.name);
+    const destinationPath = path.join(destinationDir, item.name);
 
-//     if (item.isDirectory()) {
-//       await copyDirRecursive(sourcePath, destinationPath);
-//     } else if (item.isFile()) {
-//       await fs.promises.copyFile(sourcePath, destinationPath);
-//     }
-//   }
-// }
+    if (item.isDirectory()) {
+      await copyDirRecursive(sourcePath, destinationPath);
+    } else if (item.isFile()) {
+      await fs.promises.copyFile(sourcePath, destinationPath);
+    }
+  }
+}
 
-// copyDir();
+copyDir();
